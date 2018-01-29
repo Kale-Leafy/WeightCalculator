571c901a3400f23c55826b1672334fbf2c4656f4
Kylelevy <kyle-levy@uiowa.edu>
2018-01-29 00:54:40 -0600

    Added feature to enter in weights being used and calculate the weight on the bar from there as well as cleaned up GUI

--- app/build/generated/source/r/debug/android/support/mediacompat/R.java
+++ app/build/generated/source/r/debug/android/support/mediacompat/R.java
@@ -22,13 +22,13 @@ public final class R {
         public static final int abc_action_bar_embed_tabs = 0x7f040000;
     }
     public static final class color {
-        public static final int notification_action_color_filter = 0x7f05003e;
-        public static final int notification_icon_bg_color = 0x7f05003f;
-        public static final int notification_material_background_media_default_color = 0x7f050040;
-        public static final int primary_text_default_material_dark = 0x7f050045;
-        public static final int ripple_material_light = 0x7f05004a;
-        public static final int secondary_text_default_material_dark = 0x7f05004b;
-        public static final int secondary_text_default_material_light = 0x7f05004c;
+        public static final int notification_action_color_filter = 0x7f05003f;
+        public static final int notification_icon_bg_color = 0x7f050040;
+        public static final int notification_material_background_media_default_color = 0x7f050041;
+        public static final int primary_text_default_material_dark = 0x7f050046;
+        public static final int ripple_material_light = 0x7f05004b;
+        public static final int secondary_text_default_material_dark = 0x7f05004c;
+        public static final int secondary_text_default_material_light = 0x7f05004d;
     }
     public static final class dimen {
         public static final int compat_button_inset_horizontal_material = 0x7f06004a;
@@ -79,24 +79,24 @@ public final class R {
         public static final int chronometer = 0x7f08002b;
         public static final int end_padder = 0x7f080035;
         public static final int forever = 0x7f08003b;
-        public static final int icon = 0x7f08003e;
-        public static final int icon_group = 0x7f08003f;
-        public static final int info = 0x7f080042;
-        public static final int italic = 0x7f080043;
-        public static final int line1 = 0x7f080044;
-        public static final int line3 = 0x7f080045;
-        public static final int media_actions = 0x7f080048;
-        public static final int normal = 0x7f08004e;
-        public static final int notification_background = 0x7f08004f;
-        public static final int notification_main_column = 0x7f080050;
-        public static final int notification_main_column_container = 0x7f080051;
-        public static final int right_icon = 0x7f08005a;
-        public static final int right_side = 0x7f08005b;
-        public static final int status_bar_latest_event_content = 0x7f080076;
-        public static final int text = 0x7f08007c;
-        public static final int text2 = 0x7f08007d;
-        public static final int time = 0x7f080083;
-        public static final int title = 0x7f080084;
+        public static final int icon = 0x7f08003f;
+        public static final int icon_group = 0x7f080040;
+        public static final int info = 0x7f080043;
+        public static final int italic = 0x7f080044;
+        public static final int line1 = 0x7f080045;
+        public static final int line3 = 0x7f080046;
+        public static final int media_actions = 0x7f080049;
+        public static final int normal = 0x7f080050;
+        public static final int notification_background = 0x7f080051;
+        public static final int notification_main_column = 0x7f080052;
+        public static final int notification_main_column_container = 0x7f080053;
+        public static final int right_icon = 0x7f08005b;
+        public static final int right_side = 0x7f08005c;
+        public static final int status_bar_latest_event_content = 0x7f080077;
+        public static final int text = 0x7f08007d;
+        public static final int text2 = 0x7f08007e;
+        public static final int time = 0x7f080084;
+        public static final int title = 0x7f080085;
     }
     public static final class integer {
         public static final int cancel_button_image_alpha = 0x7f090002;
--- app/src/main/java/com/kylelevydevgmail/weightcalculator/CalculatorPage.java
+++ app/src/main/java/com/kylelevydevgmail/weightcalculator/CalculatorPage.java
@@ -2,12 +2,17 @@ package com.kylelevydevgmail.weightcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.ToggleButton;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class CalculatorPage extends AppCompatActivity {
 
@@ -16,12 +21,12 @@ public class CalculatorPage extends AppCompatActivity {
     private EditText barWeightBar;
 
     private TextView weightDisplay;
-    private TextView plateVal;
-    private TextView thirtyFiveVal;
-    private TextView twentyFiveVal;
-    private TextView tenVal;
-    private TextView fiveVal;
-    private TextView twoPointFiveVal;
+    private EditText plateVal;
+    private EditText thirtyFiveVal;
+    private EditText twentyFiveVal;
+    private EditText tenVal;
+    private EditText fiveVal;
+    private EditText twoPointFiveVal;
 
     private TextView errorMessage;
 
@@ -62,11 +67,38 @@ public class CalculatorPage extends AppCompatActivity {
         toggleFive = findViewById(R.id.toggleFive);
         toggleSix = findViewById(R.id.toggleSix);
 
+        final ToggleButton[] WEIGHTTOGGLES = {toggleOne,toggleTwo,toggleThree,toggleFour,toggleFive,toggleSix};
+
+
+        //Entry bar listeners
+
+        WeightPlatesChanged platesChanged = new WeightPlatesChanged();
+
+
+        plateVal.addTextChangedListener(platesChanged);
+        thirtyFiveVal.addTextChangedListener(platesChanged);
+        twentyFiveVal.addTextChangedListener(platesChanged);
+        tenVal.addTextChangedListener(platesChanged);
+        fiveVal.addTextChangedListener(platesChanged);
+        twoPointFiveVal.addTextChangedListener(platesChanged);
+
+
+        totalWeightBar.addTextChangedListener(new WeightChanged());
 
         metaToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
+                    //The toggle is enabled
+                    for(ToggleButton currentToggle : WEIGHTTOGGLES){
+                        currentToggle.setVisibility(View.VISIBLE);
+                    }
 
+                } else {
+                    for(ToggleButton currentToggle : WEIGHTTOGGLES){
+                        currentToggle.setVisibility(View.INVISIBLE);
+                    }
+                }
             }
         });
 
@@ -81,10 +113,9 @@ public class CalculatorPage extends AppCompatActivity {
                     double val = Double.parseDouble(totalWeightText);
                     double bar = Double.parseDouble(barWeightText);
 
-                    if(val >= 45.0 && bar >= 0 && val >= bar) {
+                    if(val >= bar) {
                         boolean[] toggles = {toggleOne.isChecked(), toggleTwo.isChecked(), toggleThree.isChecked(), toggleFour.isChecked(), toggleFive.isChecked(), toggleSix.isChecked()};
                         WeightCalculator weight = new WeightCalculator(val, bar, toggles);
-                        weightDisplay.setText(weight.getWorkingVal() + "lbs");
                         totalWeightBar.setText("");
                         if(weight.isPossible()) {
                             errorMessage.setText("");
@@ -96,7 +127,7 @@ public class CalculatorPage extends AppCompatActivity {
                             twoPointFiveVal.setText("" + weight.getTwoFive());
                         }
                         else if(!weight.isPossible()){
-                            //TODO print out to the GUI that with the toggled weights, you cannot make that weight happen
+
                             plateVal.setText("" + 0);
                             thirtyFiveVal.setText("" + 0);
                             twentyFiveVal.setText("" + 0);
@@ -105,6 +136,7 @@ public class CalculatorPage extends AppCompatActivity {
                             twoPointFiveVal.setText("" + 0);
                             errorMessage.setText("Weight not possible with toggled weights");
                         }
+                        weightDisplay.setText(weight.getWorkingVal() + "lbs");
                     }else{
                         plateVal.setText("" + 0);
                         thirtyFiveVal.setText("" + 0);
@@ -114,11 +146,71 @@ public class CalculatorPage extends AppCompatActivity {
                         twoPointFiveVal.setText("" + 0);
                         errorMessage.setText("Weight entered is lower than the bar's weight alone");
                     }
+
+                } else if(!barWeightText.isEmpty() && (!plateVal.getText().equals("") || !thirtyFiveVal.getText().equals("") || !twentyFiveVal.getText().equals("")
+                        || !tenVal.getText().equals("") || !fiveVal.getText().equals("") || !twoPointFiveVal.getText().equals(""))){ //If there is a weight supplied for the bar, and at least one of the weight plates has a number, it'll calculate that weight combo
+                    double weightPlateTotal = 2 * ((45 * Double.parseDouble(plateVal.getText().toString())) + (35 * Double.parseDouble(thirtyFiveVal.getText().toString())) + (25 * Double.parseDouble(twentyFiveVal.getText().toString()))
+                            + (10 * Double.parseDouble(tenVal.getText().toString())) + (5 * Double.parseDouble(fiveVal.getText().toString())) + (2.5 * Double.parseDouble(twoPointFiveVal.getText().toString())));
+
+                    int trunc = Integer.parseInt(barWeightText) + (int)weightPlateTotal;
+                    weightDisplay.setText(trunc + "lbs");
                 }
 
             }
         });
+        
+
     }
 
+    //If the Total Weight entry bar starts taking entry, clear the slate
+    private class WeightChanged implements TextWatcher {
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            if(s.length() > 0) {
+                plateVal.setText("");
+                thirtyFiveVal.setText("");
+                twentyFiveVal.setText("");
+                tenVal.setText("");
+                fiveVal.setText("");
+                twoPointFiveVal.setText("");
+                weightDisplay.setText("");
+            }
+        }
+
+        //Must be implemented
+        @Override
+        public void afterTextChanged(Editable s) {
+        }
+
+        //Must be implemented
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+
+    }
+
+    //If any of the WeightPlates entry bars take an entry, clear the total weight bar
+    private class WeightPlatesChanged implements TextWatcher{
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            if(s.length() > 0) {
+                totalWeightBar.setText("");
+
+            }
+        }
+
+        //Must be implemented
+        @Override
+        public void afterTextChanged(Editable s) {
+        }
+
+        //Must be implemented
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+    }
 
 }
